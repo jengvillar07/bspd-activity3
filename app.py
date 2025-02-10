687087dfc0a25275ccb3f81f9a4c54a293c00f7b
Gener V. Villar <VillarGV@bsp.gov.ph>
2025-02-10 15:07:44 +0800

    Add python program

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Welcome to flask home"
+
+
+@app.route('/page1')
+def page1():
+    return "Welcome to flask home page 1"
+
+
+if __name__ =='__main__':
+    app.run(host='0.0.0.0',port=5000)
